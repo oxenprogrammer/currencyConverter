1e7d3f06d609624c36efbf97e30877b193ba1b29
oxenprogrammer <emmy1000okello@gmail.com>
2018-06-26 17:02:08 +0300

    reads user form inputs

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@
 const currencyConverterAPI = new CurrencyConverterAPI()
 const ui = new UI()
 
+// hold the form
+const form = document.getElementById('form')
+
+// add event listener
+form.addEventListener('submit', event =>{
+    event.preventDefault()
+
+    //read amount
+    const amountString = document.getElementById('amount').value
+    const amountNumber = parseFloat(amountString)
+
+    // read fromCurrency
+    const fromCurrency = document.getElementById('fromCurrency').value
+
+    // read toCurrency
+    const toCurrency = document.getElementById('toCurrency').value
+    console.log(amountNumber, fromCurrency, toCurrency)
+})
