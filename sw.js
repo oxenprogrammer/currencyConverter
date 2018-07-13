e683d02a73a33fa37c540ae14993d26c17441dc9
oxenprogrammer <emmy1000okello@gmail.com>
2018-07-13 09:03:12 +0300

    caches ./ directory

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 //install service workker
 const urls = [
+    './',
     './index.html',
     './img/favicon.jpg',
     './css/materialize.min.css',
@@ -11,23 +12,23 @@ const urls = [
     'https://free.currencyconverterapi.com/api/v5/currencies'
 ]
 self.addEventListener('install', (event) => {
-//console.log('service worker installed', event);
+    //console.log('service worker installed', event);
 
-if(!('caches' in self)) return
-event.waitUntil(
-    caches.open('v5')
-    .then(
-        (cache) => {
-            urls.forEach(url => {
-                cache.add(url).catch(
-                    error => {
-                        console.log(`This is the ${error}`)
-                    }
-                )
-            })
-        }
+    if (!('caches' in self)) return
+    event.waitUntil(
+        caches.open('v5')
+        .then(
+            (cache) => {
+                urls.forEach(url => {
+                    cache.add(url).catch(
+                        error => {
+                            console.log(`This is the ${error}`)
+                        }
+                    )
+                })
+            }
+        )
     )
-)
 });
 
 //activate service worker
@@ -39,7 +40,7 @@ self.addEventListener('activate', (event) => {
         .then((cacheKeys) => {
             return Promise.all(
                 cacheKeys.map((cacheKey) => {
-                    if(cacheKey !== CURRENT_CACHE){
+                    if (cacheKey !== CURRENT_CACHE) {
                         console.log(`Deleting cache: ${cacheKey}`)
                         return caches.delete(cacheKey)
                     }
@@ -56,26 +57,17 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         // Try the network
         fetch(event.request)
-          .then(function(res) {
+        .then(function (res) {
             return caches.open(version)
-              .then(function(cache) {
-                // Put in cache if succeeds
-                cache.put(event.request.url, res.clone());
-                return res;
-              })
-          })
-          .catch(function(err) {
-              // Fallback to cache
-              return caches.match(event.request);
-          })
+                .then(function (cache) {
+                    // Put in cache if succeeds
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                })
+        })
+        .catch(function (err) {
+            // Fallback to cache
+            return caches.match(event.request);
+        })
     );
 })
-
-    
-
-    
-        
-  
-    
-  
-  
